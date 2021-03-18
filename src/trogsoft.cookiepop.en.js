6349ac36ba0fea91b7e3c3c6e0b5b3649132f55d
Neil Bullock <neil@trogsoft.com>
2021-03-18 15:20:58 +0000

    move lang files

--- src/trogsoft.cookiepop.en.js
+++
@@ -1,7 +0,0 @@
-
-cookiepop.lang({
-    popupTitle: '',
-    popupText: 'This website uses cookies to ensure you get the best experience.',
-    popupOk: 'Accept All',
-    popupCustomize: 'Customize'
-});
